ebe5bfa54db880f24a810f7e27b849bc6c70f0a3
Tiago Brito <69848652+britotiago03@users.noreply.github.com>
2025-04-06 11:45:21 +0200

    Initialized core package

---
+++ packages/core/src/App.tsx
@@ -0,0 +1,11 @@
+// src/App.tsx
+function App() {
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>🧠 Intent.js Core</h1>
+            <p>Framework is up and running!</p>
+        </div>
+    );
+}
+
+export default App;
